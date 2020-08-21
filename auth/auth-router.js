88aaa75175d9f43b1f2b9aa39817175b22791035
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-21 11:37:17 -0400

    auth middleware working, moving on to writing tests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,7 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const Users = require("./users-model.js");
+const constants = require("./secrets");
 
 router.post("/register", (req, res) => {
   const credentials = req.body;
@@ -32,7 +33,32 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (req.body) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = signToken(user);
+
+          res.status(200).json({
+            message: "Welcome to our API",
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message, password });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
 
 router.get("/", (req, res) => {
@@ -44,5 +70,21 @@ router.get("/", (req, res) => {
       res.status(500).json({ error: error.message });
     });
 });
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department,
+  };
+
+  const secret = constants.jwtSecret || "superdupersecret";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
